73aaae36035dced6d310ca2d00cb6b2fd34d4df8
Ben Hathaway <ben@hathaway.cc>
2021-10-11 18:14:03 -0400

    Setup hook_init_http

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # haraka-plugin-prometheus
 A Haraka plugin for collecting metrics into Prometheus.
+
+## Enable Prometheus Plugin
+
+1. Enable Haraka's HTTP server (see `listen` in http.ini)
+2. Add 'prometheus' to config/plugins
+3. Configure the plugin (see below)
+
+## Config
+
+Config options are set in prometheus.json.
+
+* enabled: boolean, default true, whether to enable the plugin
--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 'use strict'
 
+const prometheus_client = require('prom-client');
+const register = prometheus_client.register;
+
 exports.register = function () {
-  this.load_prometheus_json()
+  const plugin = this;
+
+  plugin.load_prometheus_json()
+}
+
+exports.hook_init_http = function (next, server) {
+  const plugin = this;
+
+  if(plugin.cfg.prometheus.enabled == true) {
+    
+    server.http.app.get('/metrics', async (req, res) => {
+      try {
+        res.set('Content-Type', register.contentType);
+        res.end(await register.metrics());
+      } catch (ex) {
+        res.status(500).end(ex);
+      }
+    });
+    
+    // server.get('/metrics/counter', async (req, res) => {
+    //   try {
+    //     res.set('Content-Type', register.contentType);
+    //     res.end(await prometheus_client.getSingleMetricAsString('test_counter'));
+    //   } catch (ex) {
+    //     res.status(500).end(ex);
+    //   }
+    // });
+          
+    plugin.loginfo('prometheus init_http done, metrics exposed on /metrics endpoint');
+  }
+  next();
 }
 
 exports.load_prometheus_json = function () {
@@ -9,9 +42,7 @@ exports.load_prometheus_json = function () {
 
   plugin.cfg = plugin.config.get('prometheus.json', {
     booleans: [
-      '+enabled',               // plugin.cfg.main.enabled=true
-      '-disabled',              // plugin.cfg.main.disabled=false
-      '+feature_section.yes'    // plugin.cfg.feature_section.yes=true
+      '+enabled'               // plugin.cfg.main.enabled=true
     ]
   },
     function () {
