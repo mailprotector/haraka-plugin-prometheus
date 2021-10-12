d2572b49b24f09fadbf5d14055d5378a9278804c
Ben Hathaway <ben@hathaway.cc>
2021-10-12 11:22:56 -0400

    Add metrics collection

--- index.js
+++ index.js
@@ -49,9 +49,11 @@ exports.prom_deny = function (next, connection, params) {
   // let pi_params   = params[4];
   const pi_hook = params[5];
 
-  connection.logdebug(this, `prometheus deny saw: ${pi_name} ${pi_code} deny from ${pi_hook}`);
+  plugin.logdebug(`prometheus deny saw: ${pi_name} ${pi_code} deny from ${pi_hook}`, connection);
+  prometheus_client.Counter(plugin.prepare_metric_name(`${pi_hook}_total`), `Total ${pi_hook} hook calls.`, plugin.cfg.prometheus.label_names).inc(1);
+  prometheus_client.Counter(plugin.prepare_metric_name(`${pi_hook}_${pi_code}_total`), `Total ${pi_hook} hook calls with code ${pi_code}.`, plugin.cfg.prometheus.label_names).inc(1);
 
-  // TODO: Look for metrics on the connection.notes.prometheus_metrics object ie. [{type: 'counter', name 'foo', value: 1}]
+  plugin.process_metrics_from_notes(connection);
 
   next();
 }
@@ -59,11 +61,59 @@ exports.prom_deny = function (next, connection, params) {
 exports.prom_queue_ok = function (next, connection, msg) {
   const plugin = this;
 
-  connection.logdebug(this, `prometheus queue_ok saw: ${msg}`);
-
-  // TODO: Look for metrics on the connection.notes.prometheus_metrics object ie. [{type: 'counter', name 'foo', value: 1}]
+  plugin.logdebug(`prometheus queue_ok saw: ${msg}`, connection);
+  prometheus_client.Counter(plugin.prepare_metric_name(`$queue_ok_total`), `Total queue_ok hook calls.`, plugin.cfg.prometheus.label_names).inc(1);
+  
+  plugin.process_metrics_from_notes(connection);
 
   next();
+}
+
+//[{ type: 'counter', name 'foo', help: '', value: 1, label_names: ['foo', 'bar'] }]
+exports.process_metrics_from_notes = function(connection) {
+  const plugin = this;
+
+  if(connection.notes.prometheus_metrics) {
+    for (const m in connection.notes.prometheus_metrics) {
+      const metric = connection.notes.prometheus_metrics[m];
+      plugin.logdebug(`prometheus process_metrics_from_notes saw: name=${metric.name}, value=${metric.value}, help=${metric.help}, label_names=${metric.label_names}`, connection);
+      switch (metric.type) {
+        case 'counter':
+          prometheus_client.Counter(plugin.prepare_metric_name(metric.name), metric.help, plugin.prepare_label_names(metric.label_names)).inc(metric.value);
+          break;
+        case 'gauge':
+          prometheus_client.Gauge(plugin.prepare_metric_name(metric.name), metric.help, plugin.prepare_label_names(metric.label_names)).set(metric.value);
+          break;
+        case 'histogram':
+          prometheus_client.Histogram(plugin.prepare_metric_name(metric.name), metric.help, plugin.prepare_label_names(metric.label_names)).observe(metric.value);
+          break;
+        case 'summary':
+          prometheus_client.Summary(plugin.prepare_metric_name(metric.name), metric.help, plugin.prepare_label_names(metric.label_names)).observe(metric.value);
+          break;
+        default:
+          plugin.logerror(`prometheus saw unknown metric type: ${metric.type}`, connection);
+      }
+    }
+  }
+}
+
+exports.prepare_metric_name = function(metric_name) {
+  return `${plugin.cfg.prometheus.prefix}_${metric_name}`;
+}
+
+exports.prepare_label_names = function(label_names) {
+  if (plugin.cfg.prometheus.prefix && label_names) {
+    return [...plugin.cfg.prometheus.label_names, ...label_names];
+  }
+  else if (plugin.cfg.prometheus.prefix) {
+    return plugin.cfg.prometheus.label_names;
+  }
+  else if (label_names) {
+    return label_names;
+  }
+  else {
+    return [];
+  }
 }
 
 exports.load_prometheus_json = function () {
